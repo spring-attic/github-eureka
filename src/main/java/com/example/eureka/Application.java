15689f9142f6817545ccc01296d0b3a8b3c1ba39
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-09-02 14:49:29 +0200

    Changed groupid

--- src/main/java/com/example/eureka/Application.java
+++ src/main/java/com/example/eureka/Application.java
@@ -1,4 +1,4 @@
-package io.spring.cloud.samples.brewery.eureka;
+package com.example.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
